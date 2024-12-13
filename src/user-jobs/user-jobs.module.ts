2fa9ade31b2f23433fc21df92a76e90b62ea77c2
talhas6954 <talha@hegemonicsoftwares.com>
2024-12-13 20:38:54 +0500

    applying to job issue fix

--- src/user-jobs/user-jobs.module.ts
+++ src/user-jobs/user-jobs.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserJobsController } from './user-jobs.controller';
 import { UserJob } from './entities/user-job.entity';
 import { UserJobsService } from './user-jobs.service';
+import { JobPost } from '../job-post/entities/job-post.entity';
+import { User } from '../user/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserJob])],
+  imports: [TypeOrmModule.forFeature([UserJob, JobPost, User])],
   controllers: [UserJobsController],
   providers: [UserJobsService],
   exports: [UserJobsService],
--- src/user-jobs/user-jobs.service.ts
+++ src/user-jobs/user-jobs.service.ts
@@ -5,36 +5,61 @@ import { CreateUserJobDto } from './dto/create-user-job.dto';
 import { UpdateUserJobDto } from './dto/update-user-job.dto';
 import { UserJob } from './entities/user-job.entity';
 import { Status } from '../utils/constants/constants';
+import { JobPost } from '../job-post/entities/job-post.entity';
+import { User } from '../user/entities/user.entity';
 
 @Injectable()
 export class UserJobsService {
   constructor(
     @InjectRepository(UserJob)
     private userJobRepository: Repository<UserJob>,
+    @InjectRepository(JobPost)
+    private jobJobRepository: Repository<JobPost>,
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
   ) {}
 
 
-async applyToJob(createUserJobDto: CreateUserJobDto) {
-  // Check if the user has already applied for this job
-  const existingApplication = await this.userJobRepository.findOne({
-    where: {
-      jobPost: { id: createUserJobDto.jobPostId },
-      user: { id: createUserJobDto.userId },
-    },
-  });
-
-  if (existingApplication) {
-    throw new Error('User has already applied for this job');
+  async applyToJob(createUserJobDto: CreateUserJobDto) {
+    console.log(createUserJobDto, '========')
+    // Fetch the JobPost entity by jobPostId
+    const jobPost = await this.jobJobRepository.findOne({
+      where: { id: createUserJobDto.jobPostId },
+    });
+    if (!jobPost) {
+      throw new Error('Job post not found');
+    }
+  
+    // Fetch the User entity by userId
+    const user = await this.userRepository.findOne({
+      where: { id: createUserJobDto.userId },
+    });
+    if (!user) {
+      throw new Error('User not found');
+    }
+  
+    // Check if the user has already applied for this job
+    const existingApplication = await this.userJobRepository.findOne({
+      where: {
+        jobPost: { id: createUserJobDto.jobPostId },
+        user: { id: createUserJobDto.userId },
+      },
+    });
+    if (existingApplication) {
+      throw new Error('User has already applied for this job');
+    }
+  
+    // Create and save the UserJob entity
+    const userJob = this.userJobRepository.create({
+      jobPost,
+      user,
+      status: createUserJobDto.status || Status.Applied, // Default to "Applied" status if not provided
+      appliedAt: new Date(),
+    });
+  
+    return await this.userJobRepository.save(userJob);
   }
-
-  const userJob = this.userJobRepository.create({
-    ...createUserJobDto,
-    status: Status.Applied,
-    appliedAt: new Date(),
-  });
-
-  return await this.userJobRepository.save(userJob);
-}
+  
 
 
 async findByUser(userId: number) {
